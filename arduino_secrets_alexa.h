7d9f398e05b7e16da02d160ccb6450b41289fa64
mery00 <mluisa00@libero.it>
2019-12-03 12:15:32 +0100

    Files about Alexa, and antigas and fire system

---
+++ arduino_secrets_alexa.h
@@ -0,0 +1,10 @@
+//WiFi connection data
+#define SECRET_SSID "" //insert the name of the network
+#define SECRET_PASS "" //insert the password of the network
+
+//MQTT connection data
+const char* mqttUser = ""; //insert the username take from CloudMQTT
+const char* mqttPass = ""; //insert the password take from CloudMQTT
+const char* mqttServer = ""; //insert the server take from CloudMQTT
+const int mqttPort =  ; //insert the port take from CloudMQTT
+const char* mqttID = "ArduinoAlexa";
